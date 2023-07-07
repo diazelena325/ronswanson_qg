a929df5ff5f176c5693d59d0d1bd4622df645bad
Elena <elenadiaz32505@gmail.com>
2023-07-06 20:36:45 -0500

    cleaned up and better in responsiveness

--- src/App.js
+++ src/App.js
@@ -3,63 +3,60 @@ import html2canvas from "html2canvas";
 import Quote from "./components/Quote";
 
 function App() {
-  const [quote, setQuote] = useState("");
+	const [quote, setQuote] = useState("");
 
-  const fetchQuote = useCallback(async () => {
-   
-    const data = await fetch(
-      "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
-    ).then((response) => response.json());
+	const fetchQuote = useCallback(async () => {
+		const data = await fetch(
+			"https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+		).then((response) => response.json());
 
-    setQuote(data[0]);
-  }, []);
+		setQuote(data[0]);
+	}, []);
 
-  const generate = async () => {
-    fetchQuote();
-  };
+	const generate = async () => {
+		fetchQuote();
+	};
 
-  const capture = () => {
-    html2canvas(document.body, {
-      allowTaint: true,
-      scrollX: 0,
-    scrollY: -window.scrollY,
-    windowWidth: document.documentElement.offsetWidth,
-    windowHeight: document.documentElement.offsetHeight,
-    backgroundColor: null
-    }).then(function (canvas) {
-      //let canvas1 = document.createElement('canvas');
+	const capture = () => {
+		html2canvas(document.body, {
+			allowTaint: true,
+			scrollX: 0,
+			scrollY: -window.scrollY,
+			windowWidth: document.documentElement.offsetWidth,
+			windowHeight: document.documentElement.offsetHeight,
+			backgroundColor: null,
+		}).then(function (canvas) {
+			//let canvas1 = document.createElement('canvas');
 
-      let a = document.createElement("a");
-      a.download = "ronQuote.png";
-      a.href = canvas.toDataURL("image/png");
-    
-      a.click();
-     
-    });
-  };
+			let a = document.createElement("a");
+			a.download = "ronQuote.png";
+			a.href = canvas.toDataURL("image/png");
 
-  useEffect(() => {
-    fetchQuote();
-  }, [fetchQuote]);
+			a.click();
+		});
+	};
 
-  return (
-    <div className="App">
-      <div id="capture">
-        <Quote quote={quote} />
-        
-<div data-html2canvas-ignore className="buttonDiv">
+	useEffect(() => {
+		fetchQuote();
+	}, [fetchQuote]);
 
-  <button data-html2canvas-ignore onClick={generate}>
-        Generate New Quote
-      </button>
-      <button data-html2canvas-ignore onClick={capture}>
-        Save Quote
-      </button>
-</div>
-</div>
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1 className="Title">Ron Swanson Quote Generator</h1>
+			<div id="capture">
+				<Quote quote={quote} />
+
+				<div data-html2canvas-ignore className="buttonDiv">
+					<button data-html2canvas-ignore onClick={generate}>
+						Generate New Quote
+					</button>
+					<button data-html2canvas-ignore onClick={capture}>
+						Save Quote
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,25 +1,21 @@
 import React from "react";
 
-
 function Quote({ quote }) {
-  return (
-    <div className="allQuote">
-    <div className="mainChar">
-     <img src="./photos/ron.png" alt="Computer Man" className="ron" ></img>
-      </div>
-      
-<div className="quote">
-<blockquote>{quote}</blockquote>
+	return (
+		<div className="allQuote">
+			<div className="mainChar">
+				<img src="./photos/ron.png" alt="Computer Man" className="ron"></img>
+			</div>
 
-<div className="character" title="Ron Swanson">
-  - Ron Swanson
-</div>
-</div>
+			<div className="quote">
+				<blockquote>{quote}</blockquote>
 
-
-     
-    </div>
-  );
+				<div className="character" title="Ron Swanson">
+					<h3 className="signature">- Ron Swanson</h3>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Quote;
